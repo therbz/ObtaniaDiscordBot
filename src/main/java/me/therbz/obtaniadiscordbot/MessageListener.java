079a50940d09a307065d1b2142aa16cd960b12b4
TheRBZ <benhansen108@gmail.com>
2021-07-20 14:57:32 +0100

    Added some extras

--- src/main/java/me/therbz/obtaniadiscordbot/MessageListener.java
+++ src/main/java/me/therbz/obtaniadiscordbot/MessageListener.java
@@ -13,6 +13,10 @@ import java.util.concurrent.TimeUnit;
 public class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
         System.out.println("Message received: " + event.getMessage().getContentRaw());
 
         Message message = event.getMessage();
@@ -20,8 +24,8 @@ public class MessageListener extends ListenerAdapter {
             message.delete().queue();
 
             if (message.getContentRaw().split(" ").length < 2) {
-                event.getChannel().sendMessage("You must supply a suggestion! Example: `!suggest Add more rats!`").queue(botMessage -> {
-                    botMessage.delete().queueAfter(5, TimeUnit.SECONDS);
+                event.getChannel().sendMessage("<@" + event.getAuthor().getId() + ">\nYou must supply a suggestion! Example: `!suggest Add more rats!`").queue(botMessage -> {
+                    botMessage.delete().queueAfter(10, TimeUnit.SECONDS);
                 });
                 return;
             }
@@ -29,7 +33,7 @@ public class MessageListener extends ListenerAdapter {
             String suggestion = message.getContentRaw().replace("!suggest", "");
 
             EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setTitle("Towny Suggestion");
+            embedBuilder.setTitle("Towny Suggestion // !suggest");
             embedBuilder.setColor(new Color(255, 212, 0));
             embedBuilder.setDescription(suggestion);
             embedBuilder.setFooter("Suggested by " + event.getMember().getUser().getAsTag());
@@ -38,6 +42,12 @@ public class MessageListener extends ListenerAdapter {
             event.getGuild().getTextChannelById("699524366660010055").sendMessage(embedBuilder.build()).queue(botMessage -> {
                 botMessage.addReaction("✅").queue();
                 botMessage.addReaction("❌").queue();
+            });
+        }
+        else if (event.getChannel() == event.getGuild().getTextChannelById("699524366660010055")) {
+            event.getMessage().delete().queue();
+            event.getChannel().sendMessage("<@" + event.getAuthor().getId() + ">\nPlease keep suggestions discussion to <#723448278666182737>.\nIf you want to make a suggestion, use `!suggest` in this channel or <#699358412210962496>.").queue(botMessage -> {
+                botMessage.delete().queueAfter(10, TimeUnit.SECONDS);
             });
         }
     }
